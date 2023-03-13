290aaa8a7ff2794f989721c03b61e7ca6d76f56a
saviocosmo <123900209+saviocosmo@users.noreply.github.com>
2023-03-13 20:50:35 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 #chmod +x 
 
 
-git clone https://github.com/AHDSF/superbot.git
-cd superbot
+git clone https://github.com/saviocosmo/saviocosmonovo.git
+cd saviocosmonovo
 yarn install
 yarn start:init
